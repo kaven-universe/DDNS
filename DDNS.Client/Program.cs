a9586fd04ace14226dc023730aa3b7f362723f5c
Kaven <kaven@wuwenkai.com>
2025-12-17 21:45:58 +0800

    Add project files.

---
+++ DDNS.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Kaven.Standard;
+
+namespace DDNS.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utility.StartConsoleApplication<AppDynamicDomainNameServer>(new ConsoleApplicationStartOptions()
+            {
+                IoC = new IoC(),
+            });
+        }
+    }
+}
---
+++ DDNS.Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using Kaven.Standard;
+
+namespace DDNS.Server
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Utility.StartConsoleApplication<AppDynamicDomainNameServer>(new ConsoleApplicationStartOptions()
+            {
+                IoC = new IoC(),
+            });
+        }
+    }
+}
